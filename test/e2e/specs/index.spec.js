5068d46c78033516abf91f7a3d8b4d920d2fe54c
madmatvey <potehin@gmail.com>
2020-12-03 22:22:10 +0400

    fix: lint

--- test/e2e/specs/index.spec.js
+++ test/e2e/specs/index.spec.js
@@ -3,6 +3,8 @@ const Page = require('../pageObjects/main.page')
 describe('Example test', () => {
   it('should open correct app', () => {
     Page.open()
-    expect(browser).toHaveTitle('styledot.ru')
+    expect(browser).toHaveTitle(
+      'Алина Леонтьева – персональный стилист | styledot.ru'
+    )
   })
 })
