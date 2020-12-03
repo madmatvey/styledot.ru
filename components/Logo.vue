13db0ef6cb16973bdfe84f66e191a935a17addcb
madmatvey <potehin@gmail.com>
2020-12-03 20:52:21 +0400

    feat: initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <figure class="image is-128x128 StyledotAvatar">
+    <img
+      src="/styledot_avatar.jpg"
+      class="is-rounded"
+      alt="Алина Леонтьева – персональный стилист"
+    />
+  </figure>
+</template>
+
+<style>
+.StyledotAvatar {
+  animation: 1s appear;
+  justify-content: center;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">Алина Леонтьева</h1>
+      <h2 class="subtitle">персональный стилист</h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 10 10 10 10;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 150;
+  font-size: 2.5em;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 100;
+  font-size: 2em;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
