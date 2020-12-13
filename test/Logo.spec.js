c8f68d4f61b33617111097682cb9145e723e25eb
Evgene Leontiev <potehin@gmail.com>
2020-12-13 19:46:13 +0400

    feat: jest for tests (#15)

---
+++ test/Logo.spec.js
@@ -0,0 +1,18 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Logo from '@/components/Logo.vue'
+
+const factory = () => {
+  return shallowMount(Logo, {})
+}
+
+describe('Logo', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Logo)
+    expect(wrapper.vm).toBeTruthy()
+  })
+
+  test('renders properly', () => {
+    const wrapper = factory()
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
